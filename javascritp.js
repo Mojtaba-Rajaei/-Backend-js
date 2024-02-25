240f56f66c5d704dd364c3517a0a5af4f627f7a1
Mojtaba-Rajaei <Mojtabaraj@outlook.com>
2024-02-25 04:01:54 -0500

    js code

---
+++ javascritp.js
@@ -0,0 +1,40 @@
+console.log("hello world");
+console.log('hello world')
+console.log(
+    1 + 
+    2
+);
+
+console.log(
+'jfkdlsjfdfadsa',
+'dkljfds jfdlksjflkd dsjlfjds',
+'jflkdjsj jfdlksjflkd'
+)
+
+// This is a comment
+/*
+ like c++
+*/
+
+let userName = "ali";
+let userAdmin = userName;
+console.log(userAdmin);
+
+
+let newName = userName + " rezaee";
+console.log(newName);
+
+function someCode(birthday) {
+    let age = birthday.split('.');
+    console.log(age);
+    return Date.call()
+
+}
+
+let userBirth = '19.12.2000';
+let countAge = someCode(userBirth);
+console.log(userBirth);
+
+console.log(`This is countAge resualt = ${countAge}`);
+
+console.log(typeof "100" ,typeof 200 ,typeof true ,typeof Symbol(10) ,typeof NaN ,typeof null ,typeof undefined ,typeof x);
