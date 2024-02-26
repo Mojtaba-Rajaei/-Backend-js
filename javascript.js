15d1e8dfe4f6ee81105ad4f6ea43f76f80fce860
Mojtaba-Rajaei <Mojtabaraj@outlook.com>
2024-02-26 01:47:20 -0500

    I learn new somethin about oprator and type converting and ...

--- javascript.js
+++ javascript.js
@@ -38,3 +38,57 @@ console.log(userBirth);
 console.log(`This is countAge resualt = ${countAge}`);
 
 console.log(typeof "100" ,typeof 200 ,typeof true ,typeof Symbol(10) ,typeof NaN ,typeof null ,typeof undefined ,typeof x);
+
+// type convert
+console.log('2' / '10');
+
+let num = 10;
+console.log(typeof num);
+num = String(num);
+console.log(typeof num);
+
+let str = "100";
+console.log(typeof str);
+
+let convertedStr = Number(str);
+console.log(typeof convertedStr);
+
+let xNumber = "10";
+let yNumber = "20";
+
+console.log(xNumber + yNumber);
+console.log(+xNumber + +yNumber);
+
+let newX = "" + 1 + 0;
+console.log(typeof newX,newX);
+
+newX = "" + 1 - 0;
+console.log(typeof newX,newX);
+
+newX = true + false;
+console.log(typeof newX,newX);
+
+newX = 2 + 3 + "hell";
+console.log(typeof newX,newX);
+
+newX = "$" + 2 + 3;
+console.log(typeof newX,newX);
+
+newX = "$" - 2 + 3;
+console.log(typeof newX,newX);
+
+newX = 2 - 3 - "hell";
+console.log(typeof newX,newX);
+
+newX = null + 1;
+console.log(typeof newX,newX);
+
+newX = null - 1;
+console.log(typeof newX,newX);
+
+newX = undefined + 1;
+console.log(typeof newX,newX)
+
+/* prompt undefined in nodejs */
+// let newValue = prompt("Enter new value");
+// console.log(typeof newValue);
